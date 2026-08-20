85566ced834fc1e106dda31c6a65975cf090493d
s2401224 <trungngwyen@gmail.com>
2026-08-20 15:12:51 +0300

    moduuli 1 done

---
+++ moduuli 2/2.4.py
@@ -0,0 +1,11 @@
+num1 = int(input('first integer: '))
+num2 = int(input('second integer: '))
+num3 = int(input('third integer: '))
+
+total = num1 + num2 + num3
+product = num1 * num2 * num3
+average = total / 3
+
+print('The sum is:', total)
+print('The product is:', product)
+print('The average is:', average)
