9d5f86e08d2643748a45533c8273c46399721914
s2401224 <trungngwyen@gmail.com>
2026-08-21 15:12:27 +0300

    2.6 done

---
+++ moduuli 2/2.6.py
@@ -0,0 +1,16 @@
+import random
+
+
+a = random.randint(0, 9)
+b = random.randint(0, 9)
+c = random.randint(0, 9)
+
+print("3-digit code:",a,b,c)
+
+
+d = random.randint(1, 6)
+e = random.randint(1, 6)
+f = random.randint(1, 6)
+g = random.randint(1, 6)
+
+print("4-digit code:", d,e,f,g)
