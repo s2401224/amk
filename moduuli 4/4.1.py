d739a695cac249df4f60b7da1b0e9698357f2fdc
s2401224 <trungngwyen@gmail.com>
2026-09-02 16:50:53 +0300

    4.1 done

---
+++ moduuli 4/4.1.py
@@ -0,0 +1,6 @@
+num = 1
+while num <= 1000:
+    if num % 3 == 0:
+        print(num)
+    num = num + 1
+print("stopped.")
---
+++ moduuli 4/4.2.py
@@ -0,0 +1,4 @@
+num = int(input("write how many inch: "))
+
+while num >= 0:
+    print(f"{num} inch = {num * 2.54} cm.")
